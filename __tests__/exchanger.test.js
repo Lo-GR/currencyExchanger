532c20bbaac5d2eb8211a4317677847e15152558
Lo-GR <diamondintheroth@gmail.com>
2021-02-12 13:30:19 -0800

    add test for currencyTarget

--- __tests__/exchanger.test.js
+++ __tests__/exchanger.test.js
@@ -1,10 +1,15 @@
 import CurrencyExchanger from '../src/exchanger.js';
 
-describe('CurrencyExchanger.currencyCalculation', () => {
+describe('CurrencyExchanger', () => {
   
   test('should return an inputted value converted into a currency value entered, parsed to 2 decimals', () => {
     let inputtedValue = 1.25;
     let comparedValue = 2;
     expect(CurrencyExchanger.currencyCalculation(inputtedValue, comparedValue)).toEqual((2.50).toFixed(2));
   });
+  test('should return an array of in of currency name and currency conversion rate', ()=>{
+    let currencyNames = [ "USD", "AED", "AFN", "ALL",]
+    let currencyRates = [ 1, 3.6725, 77.2205, 102.0185]
+    expect(CurrencyExchanger.currencyTarget("AED", currencyNames, currencyRates));
+  })
 });
