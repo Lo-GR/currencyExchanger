e021b8e81ab1bb4e43dae0383564e511274c525b
Lo-GR <diamondintheroth@gmail.com>
2021-02-12 10:04:10 -0800

    revert changes to test error catching

--- src/exchanger.js
+++ src/exchanger.js
@@ -2,7 +2,7 @@ export default class CurrencyExchanger{
   static exchanger (){
     return new Promise(function (resolve, reject){
       let request = new XMLHttpRequest();
-      const api = `https://v6.exchangerate-api.com/v6/breakpls/latest/USD`;
+      const api = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
       request.onload = function(){
         if (this.status === 200){
           resolve(request.response);
