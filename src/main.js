e93da6146a99bf7814af121924ea80714cf12f71
Lo-GR <diamondintheroth@gmail.com>
2021-02-12 14:02:04 -0800

    add UI logic to pull conversion rate and name

--- src/main.js
+++ src/main.js
@@ -10,14 +10,22 @@ $(document).ready(function(){
   $("form#exchangeCalculator").submit(function(event){
     event.preventDefault();
     let value = parseFloat($(".numInput").val());
+    // let currencyName = $("#currency").vall();
+    let currencyName = "GEL";
     let promise = CurrencyExchanger.exchanger();
     promise.then(function(response){
       const body = JSON.parse(response);
-      let currencyNameArray = [];
-      let currencyRateArray = [];
+      let conversionNameArray = [];
+      let conversionRateArray = [];
       for (const [key, value] of Object.entries(body.conversion_rates)){
-        currencyNameArray.push(key);
-        currencyRateArray.push(value);
+        conversionNameArray.push(key);
+        conversionRateArray.push(value);
+      }
+      if (conversionNameArray.includes(currencyName)){
+        let chosenNameRate = CurrencyExchanger.currencyTarget(currencyName, conversionNameArray, conversionRateArray);
+        console.log(chosenNameRate);
+      } else {
+        $(".results").html("That's not a currency, silly");
       }
     }, function(error){
       console.log(error);
