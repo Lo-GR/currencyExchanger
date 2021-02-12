445dd381c014e4e2a1d968e6124544795aa24c74
Lo-GR <diamondintheroth@gmail.com>
2021-02-12 09:36:51 -0800

    add basic UI logic to verify if logic is working

--- src/main.js
+++ src/main.js
@@ -4,7 +4,17 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchanger from "./exchanger.js"
 
 $(document).ready(function(){
-
+  $("form#exchangeCalculator").submit(function(event){
+    event.preventDefault();
+    let promise = CurrencyExchanger.exchanger();
+    promise.then(function(response){
+      const body = JSON.parse(response);
+      console.log(body);
+    }, function(error){
+      console.log(error);
+    })
+  })
 });
